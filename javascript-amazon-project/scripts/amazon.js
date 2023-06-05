9f0116c0c08be35305f58743d94adc00833071c2
Abhishek <abhishekashyap96@gmail.com>
2023-06-05 20:56:50 +0530

    Added more products

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -25,7 +25,143 @@ const products = [
         reviews: 100
       },
       priceCents: 1299
+    },
+    {
+        image : 'images/products/black-2-slot-toaster.jpg" data-testid="product-image',
+        name : 'Black 2-Slot Toaster',
+        rating : {
+            stars : 4.5,
+            reviews : 100
+        },
+        priceCents : 1699
+    },
+    {
+        image : 'images/products/6-piece-white-dinner-plate-set.jpg' ,
+        name : '6 Piece White Dinner Plate Set' ,
+        rating : {
+            stars : 4.5 ,
+            reviews : 100
+        } ,
+        priceCents : 1299
+    },
+    {
+        image : 'images/products/backpack.jpg' ,
+        name : 'Backpack' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/bathroom-rug.jpg' ,
+        name : 'Bathroom Rug' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/black-2-slot-toaster.jpg' ,
+        name : 'Black 2-Slot Toaster' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/blackout-curtain-set-beige.webp' ,
+        name : 'Blackout Curtain Set - Beige' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/blackout-curtains-black.jpg' ,
+        name : 'Blackout Curtains - Black' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/coffeemaker-with-glass-carafe-black.jpg' ,
+        name : 'Coffeemaker with Glass Carafe - Black' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/cotton-bath-towels-teal.webp' ,
+        name : 'Cotton Bath Towels - Teal' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/countertop-blender-64-oz.jpg' ,
+        name : 'Countertop Blender - 64 oz.' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/electric-glass-and-steel-hot-water-kettle.webp' ,
+        name : 'Electric Glass and Steel Hot Water Kettle' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/liquid-laundry-detergent-plain.jpg' ,
+        name : 'Liquid Laundry Detergent - Plain' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/knit-athletic-sneakers-gray.jpg' ,
+        name : 'Knit Athletic Sneakers - Gray' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/facial-tissue-2-ply-18-boxes.jpg' ,
+        name : 'Facial Tissue - 2 Ply - 18 Boxes' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
+    },
+    {
+        image : 'images/products/duvet-cover-set-blue-twin.jpg' ,
+        name : 'Duvet Cover Set - Blue - Twin' ,
+        rating : {
+            stars : 3.5 ,
+            reviews : 190
+        },
+        priceCents : 999
     }
+  
   ];
 
   let productHTML = '';
@@ -49,7 +185,8 @@ const products = [
             </div>
 
             <div class="product-price">
-                ${(product.priceCents / 100).toFixed(2)}
+               $ ${(product.priceCents / 100).toFixed(2)} 
+              
             </div>
 
             <div class="product-quantity-container">
@@ -85,5 +222,5 @@ const products = [
 
     console.log(productHTML);
 
-    document.querySelector('.js-products-grid').innerHTML = productHTML;
+    document.querySelector('.js-products-grid').innerHTML = productHTML; //this will add the productHTML to the products-grid div in the HTML file
   
